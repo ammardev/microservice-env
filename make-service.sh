8fb9be2a88f6d253af93d53024598b0cfb64751a
Ammar Al-Khawaldeh <me@ammar.dev>
2019-12-15 14:41:11 +0200

    Clone and setup lumen automatically

--- make-service.sh
+++ make-service.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+echo "This command will create a new microservice"
+echo -e "and link it to an Nginx container on a subdomain\n"
+
 echo -n "Insert service folder name: ";
 read ans;
 service_folder_name=${ans};
-echo -n "Insert service path name: ";
+
+echo -e "\nStart installing new lumen service...\n";
+project_dir=../src/${service_folder_name}
+git clone https://github.com/3bdullahg97/lumen-mongodb.git ${project_dir};
+
+mv ${project_dir}/.env.example ${project_dir}/.env;
+docker-compose exec php bash -c "cd ${service_folder_name} && composer install && php artisan key:generate"
+
+echo -en "\nInsert service path name: ";
 read ans;
 service_url_path=${ans}
+
 template="$(cat ./nginx/service-template.conf)";
 
 GREEN='\033[0;32m'
