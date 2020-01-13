f5bc31bcab8b15ea2f921d0998470f1a296b08b4
Ammar Al-Khawaldeh <me@ammar.dev>
2020-01-13 23:57:13 +0200

    Add script to generate ssl

---
+++ generate-ssl.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+docker-compose exec nginx bash -c "certbot --nginx --non-interactive --agree-tos -m $2 -d $1" &&
+docker-compose exec nginx bash -c "echo \"0 0 * * 0 certbot renew\" >> /etc/crontabs/root" &&
+docker-compose restart nginx
+
